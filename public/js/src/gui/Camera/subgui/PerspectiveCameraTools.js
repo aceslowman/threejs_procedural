70f88f17c14b1bcab502cb7600c01d4bdce58b32
aceslowman <aceslowman@gmail.com>
2019-05-20 16:48:32 -0600

    back from outage

--- public/js/src/gui/Camera/subgui/PerspectiveCameraTools.js
+++ public/js/src/gui/Camera/subgui/PerspectiveCameraTools.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
 import InputLabel from '@material-ui/core/InputLabel';
 import Grid from '@material-ui/core/Grid';
 import Slider from '@material-ui/lab/Slider';
@@ -10,19 +10,21 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 // import Slider from 'material-ui-slider-label/Slider';
 
-const styles = theme => ({
-    textfield: {
-
-    },
-    slider: {
-
+const styles = {
+    gridBump: {
+        // position: 'relative !important',
+        // top: '10px !important'
+        marginTop: '15px'
     },
     gridOuter: {
         margin: '15px'
+    },
+    gridInner: {
+        alignItems: 'center'
     }
-});
+};
 
-class PerspectiveCameraTools extends React.Component {
+export default class PerspectiveCameraTools extends React.Component {
     constructor(props) {
         super(props);
     }
@@ -43,18 +45,27 @@ class PerspectiveCameraTools extends React.Component {
 
                     <Divider />
 
-                    <Grid item xs={4}>
-                        <TextField
-                            id="zoom-number"
-                            label="zoom"
-                            value="0"
-                            step="5"
-                            onChange={(e)=>{console.log('changed'+e)}}
-                            type="number"
-                            variant="filled" 
-                            margin="dense"
-                            style={styles.textfield}
-                        />
+                    <Grid container alignItems="center">
+                        <Grid item xs={2}>
+                            <TextField
+                                id="zoom-number"
+                                label="zoom"
+                                value="0"
+                                step="5"
+                                onChange={(e) => { console.log('changed' + e) }}
+                                type="number"
+                                variant="filled"
+                                margin="dense"
+                                style={styles.textfield}
+                            />
+                        </Grid>
+                        <Grid item xs={10} style={styles.gridBump}>
+                            <Slider 
+                                min={-10} 
+                                max={10} 
+                                value={0} 
+                            />
+                        </Grid>
                     </Grid>
 
                     <Grid item xs={4}>
@@ -200,5 +211,3 @@ class PerspectiveCameraTools extends React.Component {
         );
     }
 };
-
-export default withStyles(styles)(PerspectiveCameraTools);
